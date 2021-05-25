7dc1ea9ae93b7e295e5a3ec975d961581db82a7b
Mr. Walden <mwalden824@gmail.com>
2021-05-25 00:22:30 -0500

    Implemented the UpdateFromIMU callback function.  Task 2 complete.

--- src/QuadEstimatorEKF.cpp
+++ src/QuadEstimatorEKF.cpp
@@ -93,11 +93,30 @@ void QuadEstimatorEKF::UpdateFromIMU(V3F accel, V3F gyro)
   // (replace the code below)
   // make sure you comment it out when you add your own code -- otherwise e.g. you might integrate yaw twice
 
-  float predictedPitch = pitchEst + dtIMU * gyro.y;
-  float predictedRoll = rollEst + dtIMU * gyro.x;
-  ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
+  // float predictedPitch = pitchEst + dtIMU * gyro.y;
+  // float predictedRoll = rollEst + dtIMU * gyro.x;
+  // ekfState(6) = ekfState(6) + dtIMU * gyro.z;	// yaw
 
-  // normalize yaw to -pi .. pi
+  // // normalize yaw to -pi .. pi
+  // if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
+  // if (ekfState(6) < -F_PI) ekfState(6) += 2.f*F_PI;
+
+  //////////////////////////
+
+  Quaternion<float> dq;
+  dq = dq.IntegrateBodyRate(gyro, dtIMU);
+
+  Quaternion<float> qt;
+  qt = qt.FromEuler123_RPY(rollEst, pitchEst, ekfState(6));
+
+  Quaternion<float> qtBar;
+  qtBar = dq * qt;
+
+  float predictedPitch = qtBar.Pitch();
+  float predictedRoll = qtBar.Roll();
+  ekfState(6) = qtBar.Yaw();
+
+  // // normalize yaw to -pi .. pi
   if (ekfState(6) > F_PI) ekfState(6) -= 2.f*F_PI;
   if (ekfState(6) < -F_PI) ekfState(6) += 2.f*F_PI;
 
